4e7114655fc3f4e68a30ac73106b35c0a9740757
Jason <Jason@Pursuits-MacBook-Pro-42.local>
2019-04-08 17:57:49 -0400

    firebase support added

--- PinPoint/Controllers/LoginWithExistingViewController.swift
+++ PinPoint/Controllers/LoginWithExistingViewController.swift
@@ -7,29 +7,65 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginWithExistingViewController: UIViewController {
     
+    
     var accountExistingView = AccountExistingView()
+    var authService = AppDelegate.authservice
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        viewDidLoadLayout()
+    }
+    
+    func viewDidLoadLayout(){
         view.addSubview(accountExistingView)
         let leftBarItem = UIBarButtonItem(customView: accountExistingView.cancel)
         accountExistingView.cancel.addTarget(self, action: #selector(dismissView), for: .touchUpInside)
         self.navigationItem.leftBarButtonItem = leftBarItem
         let rightBarItem = UIBarButtonItem(customView: accountExistingView.login)
         accountExistingView.login.addTarget(self, action: #selector(loginWithCreatedAccount), for: .touchUpInside)
-        self.navigationItem.rightBarButtonItem = rightBarItem
-    }
+        self.navigationItem.rightBarButtonItem = rightBarItem    }
     
     @objc func dismissView(){
         navigationController?.popViewController(animated: true)
     }
     
     @objc func loginWithCreatedAccount(){
-        //userlogin delegate goes here
+            guard let email = accountExistingView.emailToLogin.text,
+                let password = accountExistingView.passwordToLogin.text,
+                !email.isEmpty,
+                !password.isEmpty
+                else{
+                    showAlert(title: "Error", message: "Incorrect password and/or username")
+                    return
+            }
+            authService.signInExistingAccount(email: email, password: password)
+            
     }
     
     
+}
+
+extension LoginWithExistingViewController: AuthServiceExistingAccountDelegate{
+    func didRecieveErrorSigningToExistingAccount(_ authservice: AuthService, error: Error) {
+        showAlert(title: "Error", message: error.localizedDescription)
+    }
+    
+    func didSignInToExistingAccount(_ authservice: AuthService, user: User) {
+        let containVC = ContainerController()
+        self.present(containVC, animated: true)
+        
+    }
+    
+    
+}
+
+extension LoginWithExistingViewController: UITextFieldDelegate{
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 }
