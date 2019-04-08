31220a18e4f6b9a3c73fccfa2ab1be3fa96e135a
AaronCab <Aaron@Pursuits-MacBook-Pro-17.local>
2019-04-08 12:01:20 -0400

    added files

---
+++ PinPoint/CreateAccountViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CreateAccountViewController.swift
+//  PinPoint
+//
+//  Created by Aaron Cabreja on 4/8/19.
+//  Copyright © 2019 Pursuit. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
