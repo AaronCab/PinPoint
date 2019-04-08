d9c9bc2f05647e0e8b51fa7e2a3f8d5577d70144
GMosquera1 <genesismosquera@pursuit.org>
2019-04-08 14:50:04 -0400

    hamburguer menu

--- PinPoint/Utilites/MenuOptionsCell.swift
+++ PinPoint/Utilites/MenuOptionsCell.swift
@@ -9,16 +9,47 @@
 import UIKit
 
 class MenuOptionsCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    // Mark: - Properties
+    let iconImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFit
+        iv.clipsToBounds = true
+        iv.backgroundColor = .clear
+        return iv
+    }()
+    
+    let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.text = "Pan, Lechuga, y Tomate"
+        return label
+        
+    }()
+    // Mark: - Init
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        backgroundColor = .red
+        
+        addSubview(iconImageView)
+        iconImageView.translatesAutoresizingMaskIntoConstraints = false
+        iconImageView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        iconImageView.leftAnchor.constraint(equalTo: leftAnchor, constant: 12).isActive = true
+        iconImageView.heightAnchor.constraint(equalToConstant: 24).isActive = true
+        iconImageView.widthAnchor.constraint(equalToConstant: 24).isActive = true
+        
+        addSubview(descriptionLabel)
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        descriptionLabel.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        descriptionLabel.leftAnchor.constraint(equalTo: iconImageView.rightAnchor, constant: 12).isActive = true
+        
+        
+        
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("dsada")
     }
-
+    // Mark: - Handlers
+    
 }
