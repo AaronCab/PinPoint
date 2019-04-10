2f31599159078718d0c881d85f631d37f8c7efa4
Jason <Jason@Pursuits-MacBook-Pro-42.local>
2019-04-10 12:21:21 -0400

    Some views setUp

--- PinPoint/Cells/InterestCell.swift
+++ PinPoint/Cells/InterestCell.swift
@@ -10,4 +10,34 @@ import UIKit
 
 class InterestCell: UICollectionViewCell {
     
+    
+    public lazy var collectionNameLabel: UILabel = {
+        let label = UILabel()
+        label.backgroundColor = .white
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    private func commonInit(){
+        setupCollectionNameLabel()
+    }
+    private func setupCollectionNameLabel() {
+        addSubview(collectionNameLabel)
+        collectionNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        collectionNameLabel.topAnchor.constraint(equalTo: topAnchor, constant: 10).isActive = true
+        collectionNameLabel.widthAnchor.constraint(equalTo: widthAnchor, multiplier: 0.95).isActive = true
+        collectionNameLabel.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.1).isActive = true
+        collectionNameLabel.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        
+        
+    }
 }
