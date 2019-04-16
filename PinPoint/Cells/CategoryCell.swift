2de483986d8556a1468030d9bc85c6d4f573827e
GMosquera1 <genesismosquera@pursuit.org>
2019-04-16 16:21:36 -0400

    added category model

--- PinPoint/Cells/CategoryCell.swift
+++ PinPoint/Cells/CategoryCell.swift
@@ -7,7 +7,69 @@
 //
 
 import UIKit
+import SnapKit
 
 class CategoryCell: UICollectionViewCell {
+    
+    let categoryCellContainerView: UIView = {
+        let categoryView = UIView()
+        categoryView.backgroundColor = .white
+        categoryView.layer.cornerRadius = 20
+        categoryView.layer.masksToBounds = true
+        return categoryView
+    }()
+    
+    let categoryImage: UIImageView = {
+        let catImg = UIImageView()
+        catImg.image = UIImage(named: "icons8-ask-question-25")
+        catImg.contentMode = .scaleAspectFill
+        catImg.layer.cornerRadius = 20
+        catImg.layer.masksToBounds = true
+        return catImg
+    }()
+    
+    let categoryName: UILabel = {
+        let catName = UILabel()
+        catName.text = "Event Name"
+        catName.numberOfLines = 2
+        catName.font = UIFont.systemFont(ofSize: 16, weight: .semibold)
+        catName.font = UIFont.init(name: "futura", size: 16)
+        return catName
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: UIScreen.main.bounds)
+        setupCell()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupCell() {
+        self.addSubview(categoryCellContainerView)
+        
+        categoryCellContainerView.snp.makeConstraints { (make) in
+            make.edges.equalTo(contentView)
+            
+        }
+        categoryCellContainerView.addSubview(categoryName)
+        
+        categoryCellContainerView.addSubview(categoryImage)
+        
+        categoryName.snp.makeConstraints { (make) in
+            make.top.equalTo(10)
+            make.height.equalTo(50)
+            make.left.equalTo(20)
+            make.width.equalTo(50)
+        }
+        
+        categoryImage.snp.makeConstraints { (make) in
+            make.top.equalTo(categoryName.snp.bottom).offset(15)
+            make.left.equalTo(20)
+            make.right.equalTo(-20)
+            make.height.equalTo(300)
+        }
+    }
     
 }
--- PinPoint/Views/PreferencesView.swift
+++ PinPoint/Views/PreferencesView.swift
@@ -21,27 +21,35 @@ class PreferencesView: UIView {
         gradient.colors = [firstColor.cgColor, secondColor.cgColor]
         self.layer.insertSublayer(gradient, at: 0)
     }
-
+    
+        var categoryCollectionView: UICollectionView =  {
+            let layout = UICollectionViewFlowLayout()
+            layout.minimumLineSpacing = 16
+            layout.scrollDirection = .horizontal
+            layout.itemSize = CGSize.init(width: 100, height: 100)
+            let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+            
+            cv.backgroundColor = .red
+            return cv
+        }()
+    
     var searchBar: UISearchBar = {
       let search = UISearchBar()
         search.layer.cornerRadius = 10.0
         search.backgroundColor = .clear
-        search.barTintColor = #colorLiteral(red: 0.06274510175, green: 0, blue: 0.1921568662, alpha: 1)
+        search.barTintColor = #colorLiteral(red: 0.7291600108, green: 0.03242072463, blue: 0.1055335626, alpha: 1)
+        search.tintColor = #colorLiteral(red: 0.5058823824, green: 0.3372549117, blue: 0.06666667014, alpha: 1)
         search.placeholder = "Change Your Location"
         return search
     }()
-    
-//    var categoryCollectionView: UICollectionView =  {
-//        let category = UICollectionView()
-//        category.collectionViewLayout.collectionView?.allowsMultipleSelection = true
-//        return category
-//    }()
-    
+
     var locationButton: UIButton = {
         let button = UIButton()
-        button.backgroundColor = .white
-        button.setTitleColor(.black, for: .normal)
+        button.backgroundColor = #colorLiteral(red: 0.8669512868, green: 0.8864203095, blue: 0.8792492747, alpha: 0.8279109589)
+        button.setTitleColor(#colorLiteral(red: 0.06274510175, green: 0, blue: 0.1921568662, alpha: 1), for: .normal)
         button.setTitle("Find Location", for: .normal)
+        button.titleLabel?.font = UIFont(name:
+            "futura", size: 18)
         button.isEnabled = true
         return button
     }()
@@ -50,27 +58,21 @@ class PreferencesView: UIView {
         super.init(frame: UIScreen.main.bounds)
         commonInit()
     }
-    
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         commonInit()
     }
-    
     private func commonInit(){
+        self.categoryCollectionView.register(CategoryCell.self, forCellWithReuseIdentifier: "CategoryCell")
         addGradient()
         setUpView()
     }
-    
-
-
    private func setUpView(){
     self.addSubview(searchBar)
     self.addSubview(locationButton)
     
     searchBar.snp.makeConstraints { (make) in
         make.top.equalTo(self.snp.topMargin)
-       // make.left.equalTo(10)
-       // make.right.equalTo(10)
         make.width.equalToSuperview()
     }
     
@@ -78,7 +80,7 @@ class PreferencesView: UIView {
         make.top.equalTo(searchBar.snp.bottom).offset(10)
         make.left.equalTo(20)
         make.right.equalTo(-20)
-        make.height.equalTo(100)
+        make.height.equalTo(50)
     }
     }
 
