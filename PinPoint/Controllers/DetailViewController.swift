746c8e7db122b6ebb3c9999832db8f1a282832e5
Jeffrey Almonte <jalmonte83@gmail.com>
2019-04-22 12:53:10 -0400

    cleaned up loginView code and added color to detail view small placeholder

--- PinPoint/Controllers/DetailViewController.swift
+++ PinPoint/Controllers/DetailViewController.swift
@@ -18,13 +18,13 @@ class DetailViewController: UIViewController {
     var custom: EventCreatedByUser!
     var profileOfUser: ProfileOfUser!
     var authService = AppDelegate.authservice
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.addSubview(detailView)
         updateDetailView()
-
+        
         
     }
     
@@ -42,13 +42,13 @@ class DetailViewController: UIViewController {
             
         }
         if event != nil{
-        detailView.detailImageView.kf.indicatorType = .activity
-        if event.logo?.original.url == nil{
-            detailView.detailImageView.image = UIImage(named: "PinPoint_Logo_Clear")
-        }else{
-            detailView.detailImageView.kf.setImage(with: URL(string: (event.logo?.original.url)!), placeholder: UIImage(named: "PinPoint_Logo_Clear"))
-        }
-        detailView.detailLabel.text = event.name?.text
+            detailView.detailImageView.kf.indicatorType = .activity
+            if event.logo?.original.url == nil{
+                detailView.detailImageView.image = UIImage(named: "PinPoint_Logo_Clear")
+            }else{
+                detailView.detailImageView.kf.setImage(with: URL(string: (event.logo?.original.url)!), placeholder: UIImage(named: "PinPoint_Logo_Clear"))
+            }
+            detailView.detailLabel.text = event.name?.text
             detailView.detailTextView.text = event.description?.text
             detailView.displayUserPic.isHidden = true
             detailView.displayUserLabel.isHidden = true
@@ -58,26 +58,26 @@ class DetailViewController: UIViewController {
         }
         if custom != nil{
             detailView.detailImageView.kf.indicatorType = .activity
-                detailView.detailImageView.kf.setImage(with: URL(string: (custom.photoURL.description)), placeholder: UIImage(named: "PinPoint_Logo_Clear"))
+            detailView.detailImageView.kf.setImage(with: URL(string: (custom.photoURL.description)), placeholder: UIImage(named: "PinPoint_Logo_Clear"))
             detailView.detailLabel.text = custom.displayName
             detailView.detailTextView.text = custom.eventDescription
-                if profileOfUser.photoURL != nil{
-                    detailView.displayUserPic.kf.setImage(with: URL(string: (profileOfUser.photoURL!)), placeholder: UIImage(named: "PinPoint_Logo_Clear"))
-
+            if profileOfUser.photoURL != nil{
+                detailView.displayUserPic.kf.setImage(with: URL(string: (profileOfUser.photoURL!)), placeholder: UIImage(named: "PinPoint_Logo_Clear"))
+                detailView.displayUserPic.backgroundColor = #colorLiteral(red: 1, green: 0.2061544955, blue: 0.2048995197, alpha: 0.8473619435)
             }
-                else{
-                    detailView.displayUserPic.image = UIImage(named: "PinPoint_Logo_Clear")
-                    
-                }
-                detailView.displayUserLabel.text = profileOfUser.displayName
+            else{
+                detailView.displayUserPic.image = UIImage(named: "PinPoint_Logo_Clear")
+                
+            }
+            detailView.displayUserLabel.text = profileOfUser.displayName
             detailView.messageButton.addTarget(self, action: #selector(messageVC), for: .touchUpInside)
             
-    }
+        }
     }
     
     @objc func messageVC(){
         let messageView = EventsViewController()
         
-    self.navigationController?.pushViewController(messageView, animated: true)
+        self.navigationController?.pushViewController(messageView, animated: true)
     }
 }
