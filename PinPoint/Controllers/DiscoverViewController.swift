7dc855ca8120603e53059688f5501a6e5ed84c32
Jeffrey Almonte <jalmonte83@gmail.com>
2019-04-29 17:34:53 -0400

    collection cells in all views snap to grid

--- PinPoint/Controllers/DiscoverViewController.swift
+++ PinPoint/Controllers/DiscoverViewController.swift
@@ -52,28 +52,5 @@ extension DiscoverViewController: UICollectionViewDelegate, UICollectionViewData
         cell.eventImageView.kf.setImage(with: URL(string: (currentEvent.photoURL)), placeholder: UIImage(named: "pinpointred"))
         return cell
     }
-//    func snapToNearestCell(_ collectionView: UICollectionView) {
-//        for i in 0..<collectionView.numberOfItems(inSection: 0) {
-//            let collectionViewFlowLayout = (discoverView.discoverCollectionView.collectionViewLayout as! UICollectionViewFlowLayout)
-//            let itemWithSpaceWidth = collectionViewFlowLayout.itemSize.width + collectionViewFlowLayout.minimumLineSpacing
-//            let itemWidth = collectionViewFlowLayout.itemSize.width
-//            
-//            if collectionView.contentOffset.x <= CGFloat(i) * itemWithSpaceWidth + itemWidth / 2 {
-//                let indexPath = IndexPath(item: i, section: 0)
-//                collectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
-//                break
-//            }
-//        }
-//    }
-//    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
-//        snapToNearestCell(scrollView as! UICollectionView)
-//    }
-//    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
-//        snapToNearestCell(scrollView as! UICollectionView)
-//    }
-//    func scrollViewDidEndScrollingAnimation(_ scrollView: UIScrollView) {
-//        snapToNearestCell(scrollView as! UICollectionView)
-//
-//    }
 }
 
